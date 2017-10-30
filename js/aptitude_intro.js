97596c928c98d624819df02992e7ef260b638b60
aniu <1065305323@qq.com>
2017-10-31 02:00:41 +0800

    增加公司资质介绍

---
+++ js/aptitude_intro.js
@@ -0,0 +1,48 @@
+!function () {
+    var Indexinit = 0,
+        mySwiper = null;
+    var aptitudeIntro = window.WEBBASE.extend({
+        init: function () {//初始化
+            this.swiperInit();
+        },
+        events: {
+            ".J_intro-cont-item click": "introContItemClick",//资质图片点击
+            "#J_close click": "closeClick",//关闭弹框轮播
+        },
+        /**
+         * 资质图片点击
+         */
+        introContItemClick: function (e) {
+            var selfIndex = $(e.currentTarget).index();
+            $('#J_aptitude-img-slider').fadeIn();
+            setTimeout(function () {
+                mySwiper.slideTo(selfIndex+1, 500, false);
+            }, 1000)
+
+        },
+        /**
+         * 关闭弹框轮播
+         */
+        closeClick: function () {
+            $('#J_aptitude-img-slider').hide();
+        },
+        /**
+         *  swiper初始化
+         */
+        swiperInit: function () {
+            setTimeout(function () {
+                mySwiper = new Swiper('.swiper-container', {
+                    loop: true,//环路
+                    initialSlide: Indexinit,//初始化索引
+                    // slidesPerView:2,
+                    zoom : true,
+                })
+            }, 1000)
+
+
+
+        }
+    });
+
+    aptitudeIntro.init();
+}();
